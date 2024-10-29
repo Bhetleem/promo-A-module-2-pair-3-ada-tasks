47deeb3a4e2ae9e190bffb236b23963e8076afa7
Juande <juande@Juandes-MacBook-Pro-2.local>
2024-10-29 11:21:33 +0100

    Add exercise

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 "use strict";
 
+const list = document.querySelector(".js_list");
+
+/* Por cada elemento de la lista 
+    pintar la tarea en el html */
+
+const tasks = [
+    { name: "Recoger setas en el campo", completed: true, id: 1 },
+    { name: "Comprar pilas", completed: true, id: 2 },
+    { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+    {
+      name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+      completed: false,
+      id: 4,
+    },
+  ];
+
+for(const task of tasks){
+    
+}
+
+
