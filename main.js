797f411a7d05eefd9b3a951378ff4ceeb4520eb2
Marta-ms <Marta-ms>
2024-10-30 11:25:23 +0100

    pintamos las tareas y registramos donde hace click la usuaria

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const list = document.querySelector(".js_list");
+const list = document.querySelector(".js-list");
 
 /* Por cada elemento de la lista 
     pintar la tarea en el html */
@@ -17,7 +17,20 @@ const tasks = [
   ];
 
 for(const task of tasks){
-    
+  
+  list.innerHTML +=  `<li><input type="checkbox" id="${task.id}">${task.name}</li>`
 }
 
+//Queremos que cuando la usuaria haga click en cualquiera de los checkbox la tarea se tache
+
+/*
+-Cuando la usuaria hace click la tarea se tacha
+- cuando la tarea se tacha aparece como completada*/
+
+const handleClickList = (event) => {
+  const taskComplete = parseInt(event.target.id);
+  console.log(taskComplete);
+};
+
+list.addEventListener("click", handleClickList);
 
