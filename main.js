42cf237d1b50816942430bc9d98c883920ab3a6e
Marta-ms <Marta-ms>
2024-10-31 11:33:38 +0100

    usamos fetch

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 const list = document.querySelector(".js-list");
 
+const GITHUB_USER = "Marta-ms";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+
 /* Por cada elemento de la lista 
     pintar la tarea en el html */
 
-const tasks = [
+/*const tasks = [
     { name: "Recoger setas en el campo", completed: true, id: 1 },
     { name: "Comprar pilas", completed: true, id: 2 },
     { name: "Poner una lavadora de blancos", completed: true, id: 3 },
@@ -14,12 +17,28 @@ const tasks = [
       completed: false,
       id: 4,
     },
-  ];
+  ];*/
 
-for(const task of tasks){
+  let tasks = [];
+
+  fetch(SERVER_URL)
+  .then((response) => {
+    return response.json();
+  })
+
+  .then ((data) => {
+    tasks = data.results;
+    console.log(data);
+    
+
+    for(const task of tasks){
   
-  list.innerHTML +=  `<li><input type="checkbox" id="${task.id}">${task.name}</li>`
-}
+      list.innerHTML +=  `<li><input type="checkbox" id="${task.id}">${task.name}</li>`
+    }
+    
+  })
+
+
 
 //Queremos que cuando la usuaria haga click en cualquiera de los checkbox la tarea se tache
 
